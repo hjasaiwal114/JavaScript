4c4481ca964c65f28db0c4f7626f143e02f5ea7f
himanshu jasaiwal <68457116+hjasaiwal114@users.noreply.github.com>
2023-03-24 15:38:28 +0000

    solving questions

--- exercises/daysinamonth.js
+++ exercises/daysinamonth.js
@@ -10,9 +10,23 @@
 // Use everything you learned to get to the answer
 
 const daysInMonth = (month,year) => {
+  if(month === 2) {
+    // February has 28 or 29 days depending on whether the year is a leap year
+    if ((year%4 === 0 && yeaar % 100 !== 0) || year % 400 == 0) {
+      return 29;
+    } else{
+      return 28;
+    }
+  } else if (month === 4 || month === 6 || month ===9 || month ===11) {
+    // April, June, September, and November have 30 days
+    return 30;
+  } else {
+    // all otehr months have 31 days
+    return 31;
+  }
   
 }
-
+console.log(daysInMonth(prompt('month' , 'year')))
 // Example:
 // daysInMonth(2,2018) -> 28
 // days(4,654) -> 30
